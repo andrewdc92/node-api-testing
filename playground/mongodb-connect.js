cc1506e8e5aff5507eaf8c1f2a453bdc37e82b14
andrewdc92 <andrew.cordivari@gmail.com>
2017-02-23 14:57:21 -0800

    connects to db and documents are viewable in robomongo

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,5 +6,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('connected to mongodb server');
 
+  // db.collection('Todos').insertOne({
+  //   text: 'first todo',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('unable to insert to-do', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //
+  // })
+
+  db.collection('Users').insertOne({
+    name: 'Andrew',
+    age: 25,
+    location: 'SF'
+  }, (err, result) => {
+    if (err) {
+      return console.log('unable to insert user', err);
+    }
+    console.log(JSON.stringify(result.ops));
+  })
+
   db.close();
 });
