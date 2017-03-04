0e558452d4a72559b35833eb2330e4f4839da634
andrewdc92 <andrew.cordivari@gmail.com>
2017-03-03 16:03:51 -0800

    adds post route, tested on postman and verified thru robomongo

--- server/server.js
+++ server/server.js
@@ -1,26 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  comleted: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
-  }
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+      res.status(400).send(e)
+  });
+  console.log(req.body);
 });
 
-
-var newTodo = new Todo({
-  text: 'Cook yummy dinner'
-});
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
-}, (e) => {
-  console.log('Unable to save todo');
+app.listen(3000, () => {
+  console.log('server started on port3000');
 });
